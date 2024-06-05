e9f784b66b9bdbaf00cfe8faa3b158c81e3f584d
in7hesky <anton.maiko.qa@gmail.com>
2024-06-05 20:29:50 +0300

    add logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
+from pathlib import Path
 import os
 import sys
+import logging
+import logging.config
+import yaml
 
 def create_app():
     app = Flask(__name__)
@@ -13,8 +17,21 @@ def create_app():
         dynaconf.init_app(app)
         
         app.config["SECRET_KEY"] = bytearray(app.config["SECRET_KEY"], "UTF-8")
+        _configure_logging(app, dynaconf)
         
         from . import info
         app.register_blueprint(info.info_bp)
         
         return app
+
+def _configure_logging(app, dynaconf):
+    logging_config_path = Path(app.root_path).parent / "logging_config.yaml"
+    
+    with open(logging_config_path, "r") as fh:
+        logging_config = yaml.safe_load(fh.read())
+        env_logging_level = dynaconf.settings.get("logging_level", "INFO").upper()
+        logging_level = logging.INFO if env_logging_level == "INFO" else logging.DEBUG
+    
+        logging_config["handlers"]["console"]["level"] = logging_level
+        logging_config["loggers"][""]["level"] = logging_level
+        logging.config.dictConfig(logging_config)
--- app/info/info.py
+++ app/info/info.py
@@ -1,9 +1,13 @@
 from flask import render_template
 from datetime import datetime
 from . import info_bp
+from logging import getLogger
+
+logger = getLogger(__file__)
 
 @info_bp.route("/")
 def home():
+    logger.debug("assembling home page")
     return render_template(
         "index.html", data={
         "total_visits": TotalVisits(),
@@ -12,6 +16,7 @@ def home():
 
 @info_bp.route("/about")
 def about():
+    logger.debug("assembling about page")
     return render_template("about.html")
     
 class TotalVisits:
