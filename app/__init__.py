871671565ac39b2e0547068a16ac29c8170d190a
in7hesky <anton.maiko.qa@gmail.com>
2024-06-05 19:58:24 +0300

    add debug toolbar for dev env

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from dynaconf import FlaskDynaconf
+import os
+import sys
 
 def create_app():
     app = Flask(__name__)
+    dynaconf = FlaskDynaconf(extensions_list=True)
     
     with app.app_context():
-        from . import info
+        os.environ["ROOT_PATH_FOR_DYNACONF"] = app.root_path
         
+        dynaconf.init_app(app)
+        
+        app.config["SECRET_KEY"] = bytearray(app.config["SECRET_KEY"], "UTF-8")
+        
+        from . import info
         app.register_blueprint(info.info_bp)
         
         return app
